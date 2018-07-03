fff1fb61e7a07175fe6f041862146df557f99e52
Daniel Zawadzki <hello@danielzawadzki.com>
2018-07-03 22:34:41 +0200

    Adding typing animation to the slider.

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./Slider.scss";
+import Typed from "typed.js";
 
 /**
  * Slider component.
@@ -7,13 +8,41 @@ import "./Slider.scss";
  * @returns {*}
  * @constructor
  */
-const Slider = ({}) => (
-  <h1 className="slider__text">
-    Hello, I’m{" "}
-    <span className="slider__text--hightligted">a frontend developer</span>{" "}
-    based in Warsaw, Poland
-  </h1>
-);
+class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    //Setting up typed options
+    const options = {
+      strings: [
+        "a frontend developer",
+        "a javascript enthusiast",
+        "a design enthusiast",
+        "a football lover",
+        "a star wars fan"
+      ],
+      typeSpeed: 60,
+      backSpeed: 40,
+      startDelay: 20,
+      backDelay: 1000,
+      loop: true
+    };
+
+    //Creating typed instance
+    const typed = new Typed(".slider__text--hightligted", options);
+  }
+
+  render() {
+    return (
+      <h1 className="slider__text">
+        Hello, I’m <span className="slider__text--hightligted" /> based in
+        Warsaw, Poland
+      </h1>
+    );
+  }
+}
 
 /** PropTypes */
 Slider.propTypes = {};
